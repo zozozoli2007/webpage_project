e4cf84efdbe87329c1e39f223952db1da598ef6c
Axy Hana <zoltanrigla27@gmail.com>
2025-04-09 18:15:07 +0200

    Update register.js

---
+++ user/register.js
@@ -0,0 +1,12 @@
+const username = null ;
+const name = null ;
+const email = null ;
+const passwrd = null ;
+
+var newUsername = document.getElementById('username')
+var newName = document.getElementById('name')
+var newEmail = document.getElementById('email')
+
+function register() {
+    if 
+}
